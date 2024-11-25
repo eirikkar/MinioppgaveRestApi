4482587592d0027d3ed9333d3c0cb4c6b26e8ccd
eirikkar <eirikkar@proton.me>
2024-11-25 14:33:51 +0100

    Update AppDbContext to use Tasks model and configure entity properties

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -1,13 +1,29 @@
 using Microsoft.EntityFrameworkCore;
+using TaskManager.Models;
 
 namespace TaskManager.Data;
 
 public class AppDbContext : DbContext
 {
-    public required DbSet<Task> Tasks { get; set; }
+    public required DbSet<Tasks> Tasks { get; set; }
 
     public AppDbContext(DbContextOptions<AppDbContext> options)
         : base(options) { }
 
-    protected override void OnModelCreating(ModelBuilder modelBuilder) { }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<Tasks>(entity =>
+        {
+            entity.ToTable("Tasks");
+
+            entity.HasKey(task => task.Id);
+
+            entity.Property(t => t.Title).IsRequired();
+            entity.Property(t => t.Description).IsRequired();
+            entity.Property(t => t.IsCompleted).IsRequired();
+            entity.Property(t => t.Duedate).IsRequired();
+        });
+    }
 }
