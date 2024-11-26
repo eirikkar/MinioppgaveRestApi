bdb47088b91fce15a67298718894b0ae984e9d60
eirikkar <eirikkar@proton.me>
2024-11-26 10:34:11 +0100

    Removed required from Id property

--- Models/Tasks.cs
+++ Models/Tasks.cs
@@ -2,7 +2,7 @@ namespace TaskManager.Models;
 
 public class Tasks
 {
-    public required Guid Id { get; set; }
+    public Guid Id { get; set; }
     public required string Title { get; set; }
     public required string Description { get; set; }
     public required bool IsCompleted { get; set; }
