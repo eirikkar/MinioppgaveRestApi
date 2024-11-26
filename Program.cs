b973dd0a64c99cc6045b2aadafebec0f079310fd
eirikkar <eirikkar@proton.me>
2024-11-26 10:15:28 +0100

    Configure database

--- Program.cs
+++ Program.cs
@@ -1,5 +1,18 @@
+using Microsoft.EntityFrameworkCore;
+using TaskManager.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlite("Data Source=Database/database.db")
+);
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();
+}
 
 app.MapGet("/", () => "Hello World!");
 
